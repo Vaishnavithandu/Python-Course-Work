bfca8ed35a4e970e9f05a0c99c5697f74f657f35
vaishnavi thandu <vaishnavi.thandus@gmail.com>
2025-07-02 11:42:05 +0530

    4.Python Operators

--- Classwork/4.Python Operators.py
+++ Classwork/4.Python Operators.py
@@ -2,12 +2,23 @@
 
 #1. Arthmetic operators
 
-a=10
+a=30
 b=20
-print("Addition(+):",a+b)   #Addition(+): 30
+print("Addition(+):",a+b)   #Addition(+): 50
 print("Subtraction(-):",a-b)  #Subtraction(-): -10
-print("Multiplication(*):",a*b)  # Multiplication(*): 200
-print("Division(/):",a/b)  #Division(/): 0.5
-print("Floor Divison(//):",a//b)  #Floor Divison(//): 0
+print("Multiplication(*):",a*b)  # Multiplication(*): 600
+print("Division(/):",a/b)  #Division(/): 1.5
+print("Floor Divison(//):",a//b)  #Floor Divison(//): 1
 print("Modulus(%):",a%b)  #Modulus(%): 10
-print("Exponentiation(**):",a**b)  # Exponentiation(**): 100000000000000000000
+print("Exponentiation(**):",a**b)  # Exponentiation(**): 348678440100000000000000000000
+
+#2.Comparision Operators
+
+print("Equals to(==):",a==b)  #Equals to(==): False
+print("Not Equals to(!=):",a!=b)  #Not Equals to(!=): True
+print("Greater than(>):",a>b)  # Greater than(>): True
+print("Less than(<):",a<b)  #Less than(<): False
+print("Greater than or Equals to(>=):",a>=b)  #Greater than or Equals to(>=): True
+print("Less than or Equals to(<=):",a<=b)  #Less than or Equals to(<=): False
+
+
