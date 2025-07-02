c3668b8688b7646d93f3bde72292555b280fdde0
vaishnavi thandu <vaishnavi.thandus@gmail.com>
2025-07-02 11:22:35 +0530

    4.Python Operators

---
+++ Classwork/4.Python Operators.py
@@ -0,0 +1,13 @@
+#4. Python Operators.py
+
+#1. Arthmetic operators
+
+a=10
+b=20
+print("Addition(+):",a+b)   #Addition(+): 30
+print("Subtraction(-):",a-b)  #Subtraction(-): -10
+print("Multiplication(*):",a*b)  # Multiplication(*): 200
+print("Division(/):",a/b)  #Division(/): 0.5
+print("Floor Divison(//):",a//b)  #Floor Divison(//): 0
+print("Modulus(%):",a%b)  #Modulus(%): 10
+print("Exponentiation(**):",a**b)  # Exponentiation(**): 100000000000000000000
