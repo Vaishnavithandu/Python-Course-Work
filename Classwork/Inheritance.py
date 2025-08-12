7d2fd905b507e388a7e3e1a0efcf35e971c4689b
vaishnavi thandu <vaishnavi.thandus@gmail.com>
2025-08-12 21:09:56 +0530

    Inheritance.py

---
+++ Classwork/Inheritance.py
@@ -0,0 +1,220 @@
+''' 
+
+# Inheritance :
+Ex :
+Whatsapp
+Instagram
+to access the object of parent class u no need to access from parent class u can access from child class
+
+Types of inheritance:
+
+1) Single : Parent to Child (class a to b)
+2) Multiple inheritance: multiple parents single child
+3) Multi level : single parent multiple childern / can access propertied of multiple elements/class through single class 
+   ex: 
+4) heirarchical : multiple children and single parent
+5) hybrid : whenever we have multiple inhertance we call it as hybrid
+
+syntax: 
+
+class Status:
+
+class StatusV1(Status):
+'''
+# 1) Single Inheritance: # 
+
+class A:
+    def display_a(self):
+        print("Parent Class A")
+
+class B(A):
+    def display_b(self):
+        print("Child Class B<-A")
+
+a=A()
+a.display_a()
+
+b=B()
+b.display_b()
+b.display_a()
+
+# 2) Multiple Inheritance: # 
+
+class A:
+    def display_a(self):
+        print("Parent Class A")
+
+class B:
+    def display_b(self):
+        print("Parent Class B")
+
+class C:
+    def display_c(self):
+        print("Parent Class C")
+
+class D(A,B,C):
+    def display_d(self):
+        print("Child Class A,B,C->D")
+
+d=D()
+d.display_a()
+d.display_b()
+d.display_c()
+d.display_d()
+
+# 3) Multi level Inheritance : #
+
+class A:
+    def display_a(self):
+        print("Parent Class A")
+
+class B(A):
+    def display_b(self):
+        print("Parent Class B")
+
+class C(B):
+    def display_c(self):
+        print("Parent Class C")
+
+class D(C):
+    def display_d(self):
+        print("Child Class A,B,C->D")
+
+d=D()
+d.display_a()
+d.display_b()
+d.display_c()
+d.display_d()
+
+# 4) Hierarchical Inheritance : Single Parent multiple Children #
+
+class A:
+    def display_a(self):
+        print("Parent Class A")
+
+class B(A):
+    def display_b(self):
+        print("Parent Class B")
+
+class C(A):
+    def display_c(self):
+        print("Parent Class C")
+
+class D(A):
+    def display_d(self):
+        print("Child Class A,B,C->D")
+
+b=B()
+b.display_a()
+b.display_b()
+
+c=C()
+c.display_a()
+c.display_c()
+
+d=D()
+d.display_a()
+d.display_d()
+
+# Hybrid Inheritance: whenever we have multiple inhertance  #
+
+class Status2015:
+    def view(self):
+        print("You can view status")
+    def reply(self):
+        print("You can reply")
+    def caption(self):
+        print("You can add caption")
+
+class Status2020(Status2015):
+    def uploadImg(self):
+        print("You can upload Image")
+    def uploadVid(self):
+        print("You can upload video -30 sec")
+    def privacy(self):
+        print("You can select who can see your status")
+
+class Status2023(Status2020):
+    def ProfilePrivacy(self):
+        print("You can update the profile privacy")
+    def reaction(self):
+        print("You can send reactions")           
+    def like(self):
+        print("You can like the status")
+
+
+class Status2024(Status2020):
+    def statusrings(self):
+        print("Color is added to the status display")
+    def mute(self):
+        print("You can mute the other's status")
+    def filters(self):
+        print("You can filters")
+
+    
+class Status2025(Status2023,Status2024):
+    def music(self):
+        print("You can add music")
+    def mention(self):
+        print("You can mention them")
+    def collab(self):
+        print("You can share your status from insta or fb")
+    def voicenote(self):
+        print("You can voice note")
+
+
+print("\nSheshu")       
+sheshu= Status2015()
+sheshu.view()
+sheshu.reply()
+sheshu.caption()
+
+print("\nHemanth")
+hemanth=Status2020()
+hemanth.uploadImg()
+hemanth.uploadVid()
+hemanth.privacy()
+hemanth.view()
+hemanth.reply()
+hemanth.caption()
+
+print("\nRasool")
+rasool=Status2023()
+rasool.uploadImg()
+rasool.uploadVid()
+rasool.privacy()
+rasool.view()
+rasool.reply()
+rasool.caption()
+rasool.ProfilePrivacy()
+rasool.reaction()
+rasool.like()
+
+print("\nTarit")
+tarit=Status2024()
+tarit.uploadImg()
+tarit.uploadVid()
+tarit.privacy()
+tarit.view()
+tarit.reply()
+tarit.caption()
+tarit.statusrings()
+tarit.mute()
+tarit.filters()
+
+
+print('\nVarun')
+varun=Status2025()
+varun.uploadImg()
+varun.uploadVid()
+varun.privacy()
+varun.view()
+varun.reply()
+varun.caption()
+varun.statusrings()
+varun.mute()
+varun.filters()
+varun.ProfilePrivacy()
+varun.reaction()
+varun.like()
+
---
+++ Classwork/Oops_Concepts.py
@@ -0,0 +1,124 @@
+'''
+# OOP's Concepts:
+
+whenever we want to see the outputs different from user to user(object).Based on the user we need to define the function
+ to reuse the code when we have to enhance the code
+ (to see data vary from user to user)
+
+# Drawbacks in procedural and characteristics in oops to switch to oops: #
+
+1) Encapsulation : to restrict few variables/info
+2) Abstarctions : Hiding the complex data, ex: when we have the file/image more than the fixed md the it actomaticaly reduces the size were we can see diff in quality.it hides this thing 
+3) Inheritance : when we want the properties of child class/parent class.to reuse the code 
+4) Polymorphism : same method with diff functions(actions)
+      ex: hotstart/utube premium and no premium account
+
+# Features: #
+
+1) Modularity 
+2) Maintaince
+3) scalability
+
+# this is the reason that we switch to oops concepts from Procedural 
+
+- oops is going to be run on objects and classes
+- no need to rewrite the code
+- we can reuse the code in diff platforms
+- easier to manage and organize the code
+- through encapsulation we can restrict the access to data 
+
+1) Class : 1) Attributes - data,varibles,properties
+           2) Methods - functions
+
+we use 'Class' to define a class
+      ex: Class Instagram:
+
+2) objects - the people who are using class are objects (Instance of class)
+      ex: vaishnavi = Instagram()
+          taruni = Instagram()
+
+# defing a function: #
+     
+    class Instagram:
+        def addingUsername(self,username):  --
+            print("{username} is added") ----- this thing is method(blueprint/function)
+        def uploadPost(self,posturl):
+            print(f"Added the {posturl}")
+    vaishnavi = Instagram()
+    lakshmi = Instagram()
+    taruni = Instagram()
+    
+    vaishnavi.addUsername("Vaishnavi")
+    taruni.addUsername("taruni")
+
+    vaishnavi.uploadPost("Vaishnavi","asdsd.png")
+    lakshmi.uploadPost("taruni","adssd.png")
+
+# How can the function/blueprint knows who called that function(who is adding the th username) - bcz of the self
+
+self - - we need to use the self key word to reflect/to point to the current object
+self is not a key word we can write anything instead of self (ex: abc) but mostly we use self key word
+
+# Attributes : #
+
+1) Class Attributes: The attributes which are defined by a class.fixed data/fixed calls are called class attribues
+
+ class Shopping:
+    discount=10
+    cat=['men','women','skincare','footware']
+
+2)Instant Attributes: The attributes coming from the objectsare called as Instance asttribues.data varies from user to user.It is defined from the user.
+ Ex: when user defines diff products 
+     
+ class Shopping:
+    discount=10
+    cat=['men','women','skincare','footware']
+
+     def placeorder(self,product):
+        self.product=product
+
+# Methods: #
+
+1)Instat Methods: Any method working on the instant attribute/data.
+   - in intant method we no need to mention any decorator.we define it with regular def 
+   - when we have instant method we cann't access other thinggs in other methods
+ 
+    def placeorder(self,product):
+        self.product=product
+
+2)Class Methods: whenever the method deals with the class attributes/change/modify then it's called class method. 
+
+    @classmethod ----(these are called decorators)
+    def updatediscount(cls,upd_dis):
+        cls.discount=upd_dis
+
+3)Static Method: whenever we need to do utility operations(helper function for formatting) we go with static methods.
+
+    @staticmethod
+    def formatdiscount(discount):
+        print(f"{discount}% discount is available.shop now!!")
+
+
+# How to access these methods: #
+
+Class Method: with the help of class method we can access with the methods.
+we can also access it with the object name -- but not recommended
+
+Instant Method: only instant name/only object can access the instat method
+
+class Shopping:
+    discount=10
+    cat=['men','women','skincare','footware']
+
+def placeorder(self,product):
+    self.product=product
+@classmethod ----(these are called decorators)
+    def updatediscount(cls,upd_dis):
+        cls.discount=upd_dis
+
+@staticmethod
+    def formatdiscount(discount):
+        print(f"{discount}% discount is available.shop now!!")
+
+'''
+
