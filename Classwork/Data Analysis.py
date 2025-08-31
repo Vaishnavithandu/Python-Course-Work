e2cf64f223b0ccc69e5bd808aeb1666a8debe98a
vaishnavi thandu <vaishnavi.thandus@gmail.com>
2025-09-01 01:19:55 +0530

    numpy.ipynb.py

---
+++ Classwork/Data Analysis.py
@@ -0,0 +1,91 @@
+'''
+# Data Analysis:
+
+Data Analysis is the process of collecting, cleaning, organizing, 
+and examining data to find useful information, identify patterns, 
+and support decision-making.
+
+1) To know what happend in the past(sales,..ect.)
+2) why these things are happened
+3) what to do next 
+4) what are the things needed to be improved
+
+# Types Of Data Analysis:
+
+1) Descriptive Analysis:
+
+** What happened? **
+
+- Summarizes past data using charts, graphs, reports, and dashboards.
+
+Example: Monthly sales report showing total revenue.
+
+
+2) Diagnostic Analysis:
+
+** Why did it happen? **
+
+- Looks deeper into data to find reasons for outcomes.
+
+Example: Analyzing why sales dropped (e.g., fewer customers or higher prices).
+
+
+3) Predictive Analysis:
+
+** What is likely to happen? **
+
+- Uses historical data + machine learning/statistics to forecast future outcomes.
+
+Example: Predicting future product demand based on past sales trends.
+
+
+4) Prescriptive Analysis:
+
+** What should we do? **
+
+- Suggests actions/decisions to achieve desired results.
+
+Example: Recommending marketing strategies to boost sales.
+
+
+# Why Do we need Data Analysis:
+
+We need Data Analysis to turn data into actionable knowledge 
+that drives smarter, faster, and more effective decisions.
+
+● Improves Decision-Making – Helps organizations and individuals take 
+evidence-based decisions instead of guesses..
+
+● Detects Trends & Patterns – Finds patterns in large datasets that may 
+not be obvious.
+
+● Optimizes Performance – Identifies errors, inefficiencies and areas of improvement.
+
+● Enhances Customer Experience – Personalizes services based on data insights.
+
+● Reduces Costs – Helps in budget planning and waste reduction.
+
+● Risk Management → Helps detect fraud, manage financial risks, and ensure security.
+
+● Forecasting → Predicts future outcomes (sales, demand, risks).
+
+
+# Numpy:
+
+List is not recommended - when evere we have a huge set of data as it takes 
+lots of time to perform any kind of operations.
+
+we use modules (numpy,pandas..)
+
+1) Numpy (Numerical Python):
+
+NumPy is a Python library that makes working with 
+large arrays and mathematical operations fast and easy.
+
+- NumPy (Numerical Python) is a powerful Python library for numerical computing. 
+
+- widely used for multi-dimensional arrays, mathematical operations, and linear algebra
+operations, making it essential for data analysis, machine learning, and scientific
+computing.
+
+'''
